57ceb808868b00c3e7032b8ed5b516871400c97b
My Name <myEmail@example.com>
2025-02-16 17:06:41 +0300

    Pr2

--- pr2_p_1_23_saltykova/bin/pr2_p_1_23_saltykova.dart
+++ pr2_p_1_23_saltykova/bin/pr2_p_1_23_saltykova.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-
   Directory('books').createSync(recursive: true);
-
+  DateTime now = new DateTime.now();
+  String date = "${now.day.toString()}-${now.month.toString()}-${now.year.toString()}";
+ 
   
-  String dateString = DateTime.now().toString().split(' ')[0];
-  File myFile = File('books/books_$dateString.txt');
+  File myFile = File('C:/Users/Buzun/Desktop/Practic2/pr2_p_1_23_saltykova/books/books_$date.txt');
+
 
   if (myFile.existsSync()) {
     print("Файл существует");
@@ -29,14 +30,15 @@ void main(List<String> arguments) {
   print("Автор: $avtors");
   print("Год издания: $year");
 
-  
+
   if (bookName != null && avtors != null && year != null) {
+    
     myFile.writeAsStringSync("Название: $bookName, Автор: $avtors, Год: $year\n", mode: FileMode.append);
     print("Информация о книге добавлена в файл.");
   } else {
-    print(" Ошибка, поля должны быть заполнены.");
+    print("Ошибка, поля должны быть заполнены.");
   }
 
-
+  
   print("Последнее изменение файла: ${myFile.lastModifiedSync()}");
 }
