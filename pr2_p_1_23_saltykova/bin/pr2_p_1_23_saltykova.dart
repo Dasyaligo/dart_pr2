51e9e0880d9d3993e0498e6df0e7d493ffa9ec9b
My Name <myEmail@example.com>
2025-02-16 15:31:22 +0300

    Pr2

--- pr2_p_1_23_saltykova/bin/pr2_p_1_23_saltykova.dart
+++ pr2_p_1_23_saltykova/bin/pr2_p_1_23_saltykova.dart
@@ -1,21 +1,20 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  // Создаем папку books, если она не существует
+
   Directory('books').createSync(recursive: true);
 
-  // Получаем текущую дату для имени файла
+  
   String dateString = DateTime.now().toString().split(' ')[0];
   File myFile = File('books/books_$dateString.txt');
 
-  // Проверяем, существует ли файл
   if (myFile.existsSync()) {
     print("Файл существует");
   } else {
-    print("Файла нет, он будет создан.");
+    print("Файла нет");
   }
 
-  // Запрашиваем информацию о книге
+  
   stdout.write("Введите название книги: ");
   String? bookName = stdin.readLineSync();
 
@@ -25,19 +24,19 @@ void main(List<String> arguments) {
   stdout.write("Введите год издания: ");
   String? year = stdin.readLineSync();
 
-  // Вывод информации о книге
+  
   print("Название: $bookName");
   print("Автор: $avtors");
   print("Год издания: $year");
 
-  // Записываем данные о книге в файл
+  
   if (bookName != null && avtors != null && year != null) {
     myFile.writeAsStringSync("Название: $bookName, Автор: $avtors, Год: $year\n", mode: FileMode.append);
     print("Информация о книге добавлена в файл.");
   } else {
-    print("Все поля должны быть заполнены.");
+    print(" Ошибка, поля должны быть заполнены.");
   }
 
-  // Вывод времени последнего изменения файла
+
   print("Последнее изменение файла: ${myFile.lastModifiedSync()}");
 }
